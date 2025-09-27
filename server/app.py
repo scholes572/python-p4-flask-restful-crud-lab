842e15132dd534db6e65ec6a7768ae1f595a09ee
Agostino <agostinoscholes572@gmail.com>
2025-09-27 20:19:00 +0300

    Complete

--- server/app.py
+++ server/app.py
@@ -15,41 +15,52 @@ db.init_app(app)
 api = Api(app)
 
 
+# ----------------------
+# /plants
+# ----------------------
 class Plants(Resource):
 
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+        return make_response(plants, 200)
 
     def post(self):
         data = request.get_json()
 
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
+        try:
+            new_plant = Plant(
+                name=data["name"],
+                image=data["image"],
+                price=data["price"],
+            )
 
-        db.session.add(new_plant)
-        db.session.commit()
+            db.session.add(new_plant)
+            db.session.commit()
 
-        return make_response(new_plant.to_dict(), 201)
+            return make_response(new_plant.to_dict(), 201)
+
+        except Exception as e:
+            return make_response({"error": str(e)}, 400)
 
 
-api.add_resource(Plants, '/plants')
+api.add_resource(Plants, "/plants")
 
 
+# ----------------------
+# /plants/:id
+# ----------------------
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first()
+        plant = db.session.get(Plant, id)
         if not plant:
             return make_response({"error": "Plant not found"}, 404)
 
         return make_response(plant.to_dict(), 200)
 
     def patch(self, id):
-        plant = Plant.query.filter_by(id=id).first()
+        plant = db.session.get(Plant, id)
+
         if not plant:
             return make_response({"error": "Plant not found"}, 404)
 
@@ -62,7 +73,8 @@ class PlantByID(Resource):
         return make_response(plant.to_dict(), 200)
 
     def delete(self, id):
-        plant = Plant.query.filter_by(id=id).first()
+        plant = db.session.get(Plant, id)
+
         if not plant:
             return make_response({"error": "Plant not found"}, 404)
 
@@ -71,8 +83,8 @@ class PlantByID(Resource):
         return make_response("", 204)
 
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+api.add_resource(PlantByID, "/plants/<int:id>")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
