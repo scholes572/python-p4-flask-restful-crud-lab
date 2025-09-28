05386c0424c0f995d7a04df51c7b1a2f78022b06
Agostino <agostinoscholes572@gmail.com>
2025-09-28 12:19:44 +0300

    Complete

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from server.models import db, Plant
+from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
