aa152885b10091af6e22c8e59ede81c0da3cdd0a
Agostino <agostinoscholes572@gmail.com>
2025-10-01 20:54:50 +0300

    Complete

--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -9,6 +9,7 @@ from models import Plant
 @pytest.fixture(scope='session', autouse=True)
 def setup_database():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    db.init_app(app)  # Re-init with new config
     with app.app_context():
         db.create_all()
         # Seed plants
